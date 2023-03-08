17552e71adcd5ea2f76dbe505b8760591d1eb9e8
tlee85 <114027715+tlee85@users.noreply.github.com>
2023-03-07 21:22:50 -0600

    done

--- ss&ss/js/sketch.js
+++ ss&ss/js/sketch.js
@@ -1,23 +1,75 @@
+let osc, gain, pan, ampEnv, img, shadowImg, coinSynth, shadowSynth;
 
-let osc = new Tone.AMOscillator(600, 'sine', 'sine').start()
-let gain = new Tone.Gain().toDestination();
-let pan = new Tone.Panner().connect(gain);
-let ampEnv = new Tone.AmplitudeEnvelope({
-  attack: 0.1,
-  decay: 0.2,
-  sustain: 1.0,
-  release: 0.8
-}).connect(pan);
-osc.connect(ampEnv);
+function preload() {
+  img = createImg('js/images/sonic.gif', 'sonic');
+  shadowImg = createImg('js/images/shadow.gif', 'shadow');
+  coinSynth = new Tone.MetalSynth({
+    frequency: 100,
+    envelope: {
+      attack: 0.005,
+      decay: 0.1,
+      release: 0.05
+    },
+    harmonicity: 5,
+    modulationIndex: 10,
+    resonance: 5000,
+    octaves: 1.5
+  }).toDestination();
+  shadowSynth = new Tone.MetalSynth({
+    frequency: 100,
+    envelope: {
+      attack: 0.01,
+      decay: 0.2,
+      release: 0.05
+    },
+    harmonicity: 3,
+    modulationIndex: 10,
+    resonance: 5000,
+    octaves: 0.5
+  }).toDestination();
+}
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  
+
+  osc = new Tone.AMOscillator(600, 'sine', 'sine').start();
+  gain = new Tone.Gain().toDestination();
+  pan = new Tone.Panner().connect(gain);
+  ampEnv = new Tone.AmplitudeEnvelope({
+    attack: 0.1,
+    decay: 0.2,
+    sustain: 1.0,
+    release: 0.8
+  }).connect(pan);
+  osc.connect(ampEnv);
+
+ 
+  img.position(width/4 - img.width/2, height/2 - img.height/2);
+  shadowImg.position(width*3/5 - shadowImg.width/2, height/2 - shadowImg.height/2);
 }
 
 function draw() {
-  background(220);
+  background(255,100,255);
+  textAlign(CENTER);
+  textSize(20);
+  text("Press sonic to hear a sound", width/2, 30);
+  text("Press shadow to hear a sound", width/2, 60);
 }
 
+
 function mousePressed() {
-  console.log('pressed');
+
+  if (mouseX >= img.position().x && mouseX <= img.position().x + img.width &&
+    mouseY >= img.position().y && mouseY <= img.position().y + img.height) {
+    ampEnv.triggerAttack();
+    coinSynth.triggerAttackRelease('8n');
+  } else if (mouseX >= shadowImg.position().x && mouseX <= shadowImg.position().x + shadowImg.width &&
+    mouseY >= shadowImg.position().y && mouseY <= shadowImg.position().y + shadowImg.height) {
+    shadowSynth.triggerAttackRelease('8n');
+  }
+}
+
+function mouseReleased() {
+  ampEnv.triggerRelease();
 }
